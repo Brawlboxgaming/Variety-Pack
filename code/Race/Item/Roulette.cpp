71dc0dcfda0c25f7d8f1067d37a7629fa0178eaf
Brawlboxgaming <brawlboxgaming@gmail.com>
2024-02-07 18:16:29 +0000

    Changed which function is used

--- code/Race/Item/Roulette.cpp
+++ code/Race/Item/Roulette.cpp
@@ -1,4 +1,5 @@
 #include <kamek.hpp>
+#include <VP.hpp>
 #include <game/Item/ItemSlot.hpp>
 #include <game/Item/ItemManager.hpp>
 #include <game/Race/RaceData.hpp>
@@ -6,7 +7,8 @@
 #include <Pulsar/SlotExpansion/CupsDef.hpp>
 
 int AccurateItemRoulette(Item::ItemSlotData *itemSlotData, u16 itemBoxType, u8 position, ItemId prevRandomItem, bool r7){
-    if (!Pulsar::CupsDef::IsRegsSituation()){
+    const VP::Gamemode gamemode = VP::GetGamemode();
+    if (gamemode != VP::Gamemode::RACESETTING_MODE_NONE){
         const u8 playerId = RaceInfo::sInstance->playerIdInEachPosition[position-1];
         const GameMode gameMode = RaceData::sInstance->racesScenario.settings.gamemode;
         if (gameMode != MODE_BATTLE && 
