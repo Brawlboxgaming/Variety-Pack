8db0521a71367735d98da2c2a020e733b0412c04
Brawlboxgaming <brawlboxgaming@gmail.com>
2024-03-27 17:17:15 +0000

    Fixed bad checks

--- code/Race/NoRaceProgressionTimer.cpp
+++ code/Race/NoRaceProgressionTimer.cpp
@@ -14,30 +14,33 @@ static void UpdateTimer(){
             u32 playerId = RaceData::sInstance->GetPlayerIdOfLocalPlayer(i);
             Kart::Player *player = Kart::Manager::sInstance->GetKartPlayer(playerId);
             float currentRaceCompletion = RaceInfo::sInstance->players[playerId]->raceCompletion;
-            if (currentRaceCompletion <= vp->lastRaceCompletion[playerId] && RaceData::sInstance->menusScenario.settings.gamemode != MODE_TIME_TRIAL){
-                // if the race is over, we do not want them to be respawned
-                if ((RaceInfo::sInstance->players[playerId]->stateFlags & 0x2) == 0x2){
+            if(RaceData::sInstance->menusScenario.settings.gamemode != MODE_TIME_TRIAL){
+                if (currentRaceCompletion <= vp->lastRaceCompletion[playerId]){
+                    // if the race is over, we do not want them to be respawned
+                    if ((RaceInfo::sInstance->players[playerId]->stateFlags & 0x2) == 0x2){
+                        vp->noRaceProgressionTimer[playerId] = 600;
+                    }
+                    // if someone has fallen off, we want their timer reset
+                    else if((player->link.pointers->kartStatus->bitfield0 & 0x10) == 0x10 && vp->noRaceProgressionTimer[playerId] != -1){
+                        vp->noRaceProgressionTimer[i] = 720;
+                    }
+                    // if someone is getting combo'd we don't want them to respawn, hence the damage check
+                    else if (vp->noRaceProgressionTimer[playerId] > 0
+                    && vp->noRaceProgressionTimer[playerId] != -1
+                    && (player->link.pointers->kartStatus->bitfield1 & 0x1) != 0x1
+                    ){
+                        --vp->noRaceProgressionTimer[playerId];
+                    }
+                    else if (vp->noRaceProgressionTimer[playerId] == 0){
+                        player->link.pointers->kartCollision->ActivateOob(1, 0, 0, 0);
+                        --vp->noRaceProgressionTimer[playerId];
+                    }
+                }
+                else{
                     vp->noRaceProgressionTimer[playerId] = 600;
                 }
-                // if someone has fallen off, we want their timer reset
-                else if((player->link.pointers->kartStatus->bitfield1 & 0x2) == 0x2){
-                    vp->noRaceProgressionTimer[i] = 1020;
-                }
-                // if someone is getting combo'd we don't want them to respawn, hence the damage check
-                else if (vp->noRaceProgressionTimer[playerId] > 0
-                && vp->noRaceProgressionTimer[playerId] != -1
-                && (player->link.pointers->kartStatus->bitfield1 & 0x1) != 0x1
-                ){
-                    --vp->noRaceProgressionTimer[playerId];
-                }
-                else if (vp->noRaceProgressionTimer[playerId] == 0){
-                    player->link.pointers->kartCollision->ActivateOob(1, 0, 0, 0);
-                    --vp->noRaceProgressionTimer[playerId];
-                }
+                vp->lastRaceCompletion[playerId] = currentRaceCompletion;
             }
-            // only reset the timer once they're back on track - in case of a bad respawn
-            else if (vp->noRaceProgressionTimer[playerId] == -1) vp->noRaceProgressionTimer[playerId] = 600;
-            vp->lastRaceCompletion[playerId] = currentRaceCompletion;
         }
     }
 }
