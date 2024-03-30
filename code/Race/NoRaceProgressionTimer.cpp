708054a29462a56bb42fc54e108ca8fe3b384648
Brawlbox <brawlboxgaming@gmail.com>
2024-03-30 12:31:45 +0000

    Fixed timer

--- code/Race/NoRaceProgressionTimer.cpp
+++ code/Race/NoRaceProgressionTimer.cpp
@@ -11,35 +11,41 @@ static void UpdateTimer(){
         System *vp = System::GetsInstance();
         for (int i = 0; i < GetLocalPlayerCount(); i++)
         {
-            u32 playerId = RaceData::sInstance->GetPlayerIdOfLocalPlayer(i);
+            RaceData* raceData = RaceData::sInstance;
+            RaceInfo* raceInfo = RaceInfo::sInstance;
+            u32 playerId = raceData->GetPlayerIdOfLocalPlayer(i);
             Kart::Player *player = Kart::Manager::sInstance->GetKartPlayer(playerId);
-            float currentRaceCompletion = RaceInfo::sInstance->players[playerId]->raceCompletion;
-            if(RaceData::sInstance->menusScenario.settings.gamemode != MODE_TIME_TRIAL){
-                if (currentRaceCompletion <= vp->lastRaceCompletion[playerId]){
-                    // if the race is over, we do not want them to be respawned
-                    if ((RaceInfo::sInstance->players[playerId]->stateFlags & 0x2) == 0x2){
+            float currentRaceCompletion = raceInfo->players[playerId]->raceCompletion;
+            RaceStage stage = raceInfo->stage;
+            if(raceData->menusScenario.settings.gamemode != MODE_TIME_TRIAL){
+                // if the race has not started, we do not want the timer to be changed from -1 (globally)
+                if (stage >= RACESTAGE_RACE){
+                    if (currentRaceCompletion <= vp->lastRaceCompletion[playerId]){
+                        // if the race is over, we do not want them to be respawned (on per player basis)
+                        if ((raceInfo->players[playerId]->stateFlags & 0x2) == 0x2){
+                            vp->noRaceProgressionTimer[playerId] = -1;
+                        }
+                        // if someone has fallen off, we want their timer reset
+                        else if((player->link.pointers->kartStatus->bitfield0 & 0x10) == 0x10 && vp->noRaceProgressionTimer[playerId] != -1){
+                            vp->noRaceProgressionTimer[i] = 720;
+                        }
+                        // if someone is getting combo'd we don't want them to respawn, hence the damage check
+                        else if (vp->noRaceProgressionTimer[playerId] > 0
+                        && vp->noRaceProgressionTimer[playerId] != -1
+                        && (player->link.pointers->kartStatus->bitfield1 & 0x1) != 0x1
+                        ){
+                            --vp->noRaceProgressionTimer[playerId];
+                        }
+                        else if (vp->noRaceProgressionTimer[playerId] == 0){
+                            player->link.pointers->kartCollision->ActivateOob(1, 0, 0, 0);
+                            --vp->noRaceProgressionTimer[playerId];
+                        }
+                    }
+                    else{
                         vp->noRaceProgressionTimer[playerId] = 600;
                     }
-                    // if someone has fallen off, we want their timer reset
-                    else if((player->link.pointers->kartStatus->bitfield0 & 0x10) == 0x10 && vp->noRaceProgressionTimer[playerId] != -1){
-                        vp->noRaceProgressionTimer[i] = 720;
-                    }
-                    // if someone is getting combo'd we don't want them to respawn, hence the damage check
-                    else if (vp->noRaceProgressionTimer[playerId] > 0
-                    && vp->noRaceProgressionTimer[playerId] != -1
-                    && (player->link.pointers->kartStatus->bitfield1 & 0x1) != 0x1
-                    ){
-                        --vp->noRaceProgressionTimer[playerId];
-                    }
-                    else if (vp->noRaceProgressionTimer[playerId] == 0){
-                        player->link.pointers->kartCollision->ActivateOob(1, 0, 0, 0);
-                        --vp->noRaceProgressionTimer[playerId];
-                    }
+                    vp->lastRaceCompletion[playerId] = currentRaceCompletion;  
                 }
-                else{
-                    vp->noRaceProgressionTimer[playerId] = 600;
-                }
-                vp->lastRaceCompletion[playerId] = currentRaceCompletion;
             }
         }
     }
@@ -48,7 +54,7 @@ static RaceFrameHook UpdateRaceProgTimerHook(UpdateTimer);
 
 static void ResetTimer(){
     if (!Pulsar::CupsConfig::IsRegsSituation()){
-        for (int i = 0; i < 4; i++) System::GetsInstance()->noRaceProgressionTimer[i] = 1020;
+        for (int i = 0; i < 4; i++) System::GetsInstance()->noRaceProgressionTimer[i] = -1;
     }
 }
 static RaceLoadHook ResetRaceProgTimerHook(ResetTimer);
